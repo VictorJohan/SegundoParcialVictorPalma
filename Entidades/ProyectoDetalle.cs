96c9741acc45560030aad7b023e3195d73160e9c
Victor Johan <Vicjohan23@gmail.com>
2020-07-06 18:26:59 -0400

    HACIENDO COMMIT (PUNTO DE CONTROL)

---
+++ Entidades/ProyectoDetalle.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SegundoParcialVictorPalma.Entidades
+{
+    public class ProyectoDetalle
+    {
+        [Key]
+        public int Id { get; set; }
+        public int ProyectoId { get; set; }
+        public int TipoId { get; set; }
+        public string TipoTarea { get; set; }
+    }
+}
---
+++ Entidades/Proyectos.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace SegundoParcialVictorPalma.Entidades
+{
+    public class Proyectos
+    {
+        [Key]
+        public int ProyectoId { get; set; }
+        public DateTime Fecha { get; set; } = DateTime.Now;
+        public string DescripcionProyecto { get; set; }
+    }
+}
